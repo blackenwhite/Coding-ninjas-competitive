c485d1b9451dddccd55670613a41649dbf4936c7
Nabajyoti Majumdar <52462493+blackenwhite@users.noreply.github.com>
2020-04-04 20:59:17 +0530

    Update Zalgo.cpp

--- String/Zalgo.cpp
+++ String/Zalgo.cpp
@@ -1,53 +1,49 @@
-/*
-
-				Name: Mehul Chaturvedi
-				IIT-Guwahati
-
-*/
-
-/*
-				PROBLEM STATEMENT
-
-*/
-
 #include <bits/stdc++.h>
-using namespace std;
+using namespace std;  //                       Nabajyoti Majumdar
 
-typedef long long ll;
-typedef unordered_map<int, int> umapii;
-typedef unordered_map<int, bool> umapib;
-typedef unordered_map<string, int> umapsi;
-typedef unordered_map<string, string> umapss;
-typedef map<string, int> mapsi;
-typedef map<pair<int, int>, int> mappiii;
-typedef map<int, int> mapii;
-typedef pair<int, int> pii;
-typedef pair<long long, long long> pll;
-typedef unordered_set<int> useti;
-
-#define uset unordered_set
-#define it iterator
-#define mp make_pair
-#define pb push_back
-#define all(x) (x).begin(), (x).end()
-#define f first
-#define s second
-#define MOD 1000000007
-
-
-int main( int argc , char ** argv )
-{
-	ios_base::sync_with_stdio(false) ; 
-	cin.tie(NULL) ; 
-	
-	
-	
-
-
-
-	return 0 ; 
-
-
-
+void buildZ(int* Z, string str){
+  int l=0;int r=0;
+  int n=str.length();
+  for (int i=1;i<n;i++){
+    if(i>r){
+      l=i;
+      r=i;
+      while(r<n && str[r-l]==str[r]){
+        r++
+      }
+      Z[i]=r-l;
+      r--;
+    }else{
+      int k=i-l;
+      if(Z[k]<=r-i){
+        Z[i]=Z[k];
+      }else{
+        l=i;
+        while (r<n && str[r-l]==str[r]){
+          r++;
+        }
+        Z[i]=r-l;
+        r--;
+      }
+    }
+  }
 }
+
+void searchstring(string text,string pattern){
+  string str=pattern+"$"+text;
+  int n=str.length();
+  int* Z=new int[n]();
+  buildZ(Z,str);
+  for (int i=0;i<n;i++){
+    cout<<i-pattern.length()-1<<endl;
+  }
+}
+
+int main(){
+  string text="abcdsafbcdfasbcda";
+  string pattern="bcd";
+  searchstring(text,pattern);
+  return 0;
+}
+
 
