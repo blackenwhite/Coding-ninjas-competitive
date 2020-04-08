d539bdae5d02694d62184f8d3e12c45b5fa8a984
Nabajyoti Majumdar <52462493+blackenwhite@users.noreply.github.com>
2020-04-08 23:02:55 +0530

    Update Connectedhorses.cpp

--- Advanced Graphs/Connectedhorses.cpp
+++ Advanced Graphs/Connectedhorses.cpp
@@ -1,7 +1,7 @@
 /*
 
-				Name: Mehul Chaturvedi
-				IIT-Guwahati
+				Nabajyoti Majumdar
+				
 
 */
 /*
@@ -42,207 +42,83 @@ SAMPLE INPUT
 4 4 4
 1 1
 1 2
-3 1
+3 1                             Inspired from PRADEEP YADAV
 4 4
 SAMPLE OUTPUT
 4
 2
 */
 
-
-#include <bits/stdc++.h>
-
+#include<bits/stdc++.h>
 using namespace std;
-#define pii pair<ll,ll>
-#define MOD 1000000007
-#define ll long long
+#define p int(1e9+7)
+int mat[1001][1001],n,m;
+#define ll long long int
 
-ll dfs(ll i, ll j, vector<pii>** graph, ll** visited){
-	visited[i][j] = 1;
-	ll answer = 1;
+void input();
+void func();
+ll fac(ll);
+ll bfs(int,int);
 
-	for(ll k = 0; k < graph[i][j].size(); k++){
-        ll x = graph[i][j][k].first;
-        ll y = graph[i][j][k].second;
-        if(!visited[x][y]){
-            answer = (answer+dfs(x, y, graph, visited))%MOD;
-        }
-    }
-
-    return answer;
-
-	// ll count = 0;
-	// for (ll i = 0; i < graph[sx][sy].size(); ++i)
-	// {
-	// 	ll left = (graph[sx][sy].at(i)).first;
-	// 	ll right = (graph[sx][sy].at(i)).second;
-	// 	if (visited[left][right] == 0)
-	// 	{
-	// 		visited[left][right] = 1;
-	// 		count = (count+1)%MOD;
-	// 	}
-		
-	// }
-
-	// return count%MOD;
-}
-
-void fill_vector(vector<pii>** graph, ll** board, ll m, ll n){
-	for (ll i = 0; i < n; ++i)
-	{
-		for (ll j = 0; j < m; ++j)
-		{
-			if (board[i][j] == 1)
-			{
-				graph[i][j].push_back(make_pair(i, j));
-
-				if (i+2<n && j+1<m && board[i+2][j+1] == 1)
-				{
-					//return ;
-					graph[i][j].push_back(make_pair(i+2, j+1));
-				}
-		//return ;
-
-
-				if (i+2<n && j-1>=0 && board[i+2][j-1] == 1)
-				{
-					graph[i][j].push_back(make_pair(i+2, j-1));
-				}
-
-				if (i-2>=0 && j+1<m && board[i-2][j+1] == 1)
-				{
-					graph[i][j].push_back(make_pair(i-2, j+1));
-				}
-
-				if (i-2>=0 && j-1>=0 && board[i-2][j-1] == 1)
-				{
-					graph[i][j].push_back(make_pair(i-2, j-1));
-				}
-
-				if (i+1<n && j+2<m && board[i+1][j+2] == 1)
-				{
-					graph[i][j].push_back(make_pair(i+1, j+2));
-				}
-
-				if (i+1<n && j-2>=0 && board[i+1][j-2] == 1)
-				{
-					graph[i][j].push_back(make_pair(i+1, j-2));
-				}
-
-				if (i-1>=0 && j+2<m && board[i-1][j+2] == 1)
-				{
-					graph[i][j].push_back(make_pair(i-1, j+2));
-				}
-
-				if (i-1>=0 && j-2>=0 && board[i-1][j-2] == 1)
-				{
-					graph[i][j].push_back(make_pair(i-1, j-2));
-				}
-			}
-		}
-	}
-
-	return;
-}
-
-
-int main( int argc , char ** argv )
+int main()
 {
-
-	ios_base::sync_with_stdio(false) ; 
-	cin.tie(NULL) ; 
-	
-	//To get the factorial
-	ll *factorial = new ll[1000000];
-    factorial[1] = 1;
-    for(ll i = 2; i < 1000000; i++){
-    	factorial[i] = (factorial[i-1]*i)%MOD;
-    }
-
-    //Main part of the input
-	ll t;
-	cin>>t;
-	while(t--){
-		ll n, m;
-		ll q;
-		cin>>n>>m>>q;
-
-		//Create a 2d array for board
-		ll** board = new ll*[n];
-		for (ll i = 0; i < n; ++i)
-		{
-			board[i] = new ll[m];
-			for (ll j = 0; j < m; ++j)
-			{
-				board[i][j] = 0;
-			}
-		}
-
-		//Fill the board
-		while(q--){
-			ll x, y;
-			cin>>x>>y;
-			board[x-1][y-1] = 1;
-		}
-
-
-		//Creating a graph 2d array
-		vector<pii>** graph = new vector<pii>*[(n)];
-		for (ll i = 0; i < n; ++i)
-		{
-			graph[i] = new vector<pii>[m];
-		}
-
-		//Fill the graph
-		fill_vector(graph, board, m, n);
-
-		// for (ll i = 0; i < n; ++i)
-		// {
-		// 	for (ll j = 0; j < m; ++j)
-		// 	{
-		// 		if (graph[i][j].size()!=0)
-		// 		{
-		// 			for (ll k = 0; k < graph[i][j].size(); ++k)
-		// 			{
-		// 				cout<<graph[i][j][k].first<<" "<<graph[i][j][k].second<<"    ";
-		// 			}
-		// 			cout <<'\n';
-		// 		}
-		// 	}
-		// }
-
-		//Count and multiply the number of elements
-		ll** visited = new ll*[n];
-		for (ll i = 0; i < n; ++i)
-		{
-			visited[i] = new ll[m];;
-			for (ll j = 0; j < m; ++j)
-			{
-				visited[i][j] = 0;
-			}
-		}
-
-		ll ans = 1;
-		for (ll i = 0; i < n; ++i)
-		{
-			for (ll j = 0; j < m; ++j)
-			{
-				if (visited[i][j] == 0 && board[i][j] == 1)
-				{
-					// cout << i<<" "<<j<<" "<<"    "<<dfs(i, j, graph, visited, m, n) << '\n';
-					// cout << "Here "<<factorial[2] << '\n';
-					// cout << i<<" "<<j<<" "<<"    "<<dfs(i, j, graph, visited, m, n) << '\n';
-
-					ans = (ans*factorial[dfs(i, j, graph, visited)])%MOD;
-				}
-				
-			}
-		}
-
-		cout << ans << '\n';
-	}
-
-
-	return 0 ; 
-
+	int t;
+    cin>>t;
+    while(t--) input();
+	return 0;
 }
+
+void input(){
+    int q;
+    cin>>n>>m>>q;
+    for (int i=1;i<=n;i++)
+        for (int j=1;j<=m;j++) 
+            mat[i][j]=0;
+    
+    int x,y;
+    while(q--){
+        cin>>x>>y;
+        mat[x][y]=1;
+        
+    }
+    func();
+    
+}
+
+void func(){
+    long long ans=1,t;
+    for(int i=1;i<=n;i++)
+        for(int j=1;j<=m;j++){
+           if(mat[i][j]==1){
+               t=bfs(i,j);
+               ans=(ans*fac(t))%p;
+           } 
+        }
+    cout<<ans<<endl;
+}
+
+
+ll fac(ll t)
+{
+	ll ans=1;
+	for(ll i=1;i<=t;i++)ans=(ans*i)%p;
+	return ans;
+}
+
+ll bfs(int i,int j)
+{
+	ll t=1;
+	mat[i][j]=2;
+	// the eight conditions
+	if(i-2>=1 && j+1<=m && mat[i-2][j+1]==1)t+=bfs(i-2,j+1);
+	if(i-1>=1 && j+2<=m && mat[i-1][j+2]==1)t+=bfs(i-1,j+2);
+	if(i+1<=n && j+2<=m && mat[i+1][j+2]==1)t+=bfs(i+1,j+2);
+	if(i+2<=n && j+1<=m && mat[i+2][j+1]==1)t+=bfs(i+2,j+1);
+	
+	if(i-2>=1 && j-1>=1 && mat[i-2][j-1]==1)t+=bfs(i-2,j-1);
+	if(i-1>=1 && j-2>=1 && mat[i-1][j-2]==1)t+=bfs(i-1,j-2);
+	if(i+1<=n && j-2>=1 && mat[i+1][j-2]==1)t+=bfs(i+1,j-2);
+	if(i+2<=n && j-1>=1 && mat[i+2][j-1]==1)t+=bfs(i+2,j-1);
+	return t;
+}
+
